fa153a2a994868fef2ba174d59d7722f6afa5a06
Kyle Ledoux <kaledoux@gmail.com>
2021-07-22 11:31:41 -0400

    update to Strategy rollout determination; removed test code

--- lib/ruby_sdk/mod/handle_undefined_feature.rb
+++ lib/ruby_sdk/mod/handle_undefined_feature.rb
@@ -1,9 +1,9 @@
 module HandleUndefinedFeature
 	def self.handle(key, default_value)
 		unless default_value.nil?
-			puts("Warning: Could not get #{key} from features, using provided default value!")
+			puts("Warning: Could not get '#{key}' from features, using provided default value!")
 			return default_value
 		end
-		raise("Error: #{key} does not exist, cannot get get feature!");
+		raise("Error: flag with title '#{key}' does not exist, cannot get get feature!");
 	end
 end
